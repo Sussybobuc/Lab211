a617b99e65312478b92d9c701fb16bdd0d52669d
HUỲNH LÊ NHẤT HUY <itwasajokestop@gmail.com>
2026-09-08 20:50:54 +0700

    First commit

---
+++ LAB211/src/main/java/Week1/Bubble_sort.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week1;
+
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author ASUS
+ */
+public class Bubble_sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int Arr[] = new int[n]; 
+    }
+}

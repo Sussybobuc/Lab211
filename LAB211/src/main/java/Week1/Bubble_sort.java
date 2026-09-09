c29f6397e67c1de9b8ff29f69c55dcab005ee573
HUỲNH LÊ NHẤT HUY <itwasajokestop@gmail.com>
2026-09-09 22:27:40 +0700

    Bubble sort

--- LAB211/src/main/java/Week1/Bubble_sort.java
+++ LAB211/src/main/java/Week1/Bubble_sort.java
@@ -6,14 +6,50 @@ package Week1;
 
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
+
 /**
  *
  * @author ASUS
  */
 public class Bubble_sort {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int Arr[] = new int[n]; 
+        int n = -1;
+        boolean flag = false;
+        while (!flag) {
+            try {
+                System.out.println("Enter number of array:");
+                n = sc.nextInt();
+                if (n > 0) {
+                    flag = true;
+                }
+            } catch (RuntimeException e) {
+                System.out.println("Wrong input");
+                sc.next();
+            }
+        }
+        Random rand = new Random();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(10 - 1) + 1;
+        }
+        System.out.print("Unsorted array: " + Arrays.toString(arr));
+
+        boolean notSwapped;
+        do {
+            notSwapped = true;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                    notSwapped = false;
+                }
+            }
+        } while (!notSwapped);
+                System.out.print("\nSorted array: " + Arrays.toString(arr));
+
     }
 }
